e642e80a8d9c0f59b2db187171fea7ec998c75ba
mdrony5134 <rony830388@gamil.com>
2023-10-04 00:24:55 +0600

    learning variables

---
+++ 01_BasicJs/01_variables.js
@@ -0,0 +1,15 @@
+const userId = 14242;
+let userEmail = "rony@google.com"
+var userPassword = "123456"
+userCountry = "bangladesh"
+
+// userId = 254
+
+console.log(userId);
+
+/* 
+   prefer not use to var 
+   because of issue in block scope and functional scope  
+*/
+
+console.table([userEmail, userPassword, userCountry])
