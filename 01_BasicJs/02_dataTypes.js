4ec600cb49974fcb91a7406ac27210323776e9a1
mdrony5134 <rony830388@gamil.com>
2023-10-04 00:56:30 +0600

    learning datatypes

---
+++ 01_BasicJs/02_dataTypes.js
@@ -0,0 +1,20 @@
+"user strict" ; // treat all js code as a newer version
+
+// alert(3 + 2) // error: we are using nodejs, not use browser 
+
+let name = "rony"
+let age = 19
+let isLoggedIn = true / false ;
+
+
+// this are permeative data type
+// number => 2 to power 53
+// string => ""
+// boolean => true , false
+// bigint
+// null
+// undefined
+// symbol => unique
+
+console.log(typeof null) // output : object
+console.log(typeof undefined) // output : undefined
