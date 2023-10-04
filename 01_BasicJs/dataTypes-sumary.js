7c1995d693238419fee496cdc17e772dd0b63176
mdrony5134 <rony830388@gamil.com>
2023-10-04 14:30:53 +0600

    sumary datatypes

---
+++ 01_BasicJs/dataTypes-sumary.js
@@ -0,0 +1,30 @@
+// primitive data type  ---- its call by value
+
+// 7 types: number, string, boolean, null, undefined, symbol, bigInt
+
+const score =100 
+const scoreValue = 100.5
+const isLoggedIn = false / true
+const name = "rony"
+let userEmail;
+const temp = null;  // object type
+
+// Reference type (Non Primitive)
+// array , object, function
+
+// array
+const frindsName = ["shihab", "Riyad", "firad"]
+
+
+// object
+
+const myObj = {
+    name: "rony",
+    age : 20
+}
+
+// function
+
+function hablu(){
+
+}
