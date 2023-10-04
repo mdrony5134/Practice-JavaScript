dc6785ebe8adb1b92ad22d1b0da738cd6c7ded15
mdrony5134 <rony830388@gamil.com>
2023-10-04 12:26:14 +0600

    comparision

---
+++ 01_BasicJs/04_comparision.js
@@ -0,0 +1,12 @@
+// console.log(null > 0)          // false 
+// console.log(null == 0)          // false
+// console.log(null >= 0)          //true
+// console.log(null < 0)          //false
+
+
+// console.log(undefined > 0)     // false
+// console.log(undefined == 0)     //false
+// console.log(undefined >= 0)     //false
+// console.log(undefined < 0)     // false
+
+console.log("1" > 2)               // false
