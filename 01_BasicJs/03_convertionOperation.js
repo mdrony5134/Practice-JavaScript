1336f85f4abace8fb1fcb644fa5d9f01b916acb8
mdrony5134 <rony830388@gamil.com>
2023-10-04 11:40:31 +0600

    convertion and operations

---
+++ 01_BasicJs/03_convertionOperation.js
@@ -0,0 +1,21 @@
+let score = null
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let scoreNumber = Number(score)
+
+// console.log(typeof scoreNumber);
+// console.log(scoreNumber);
+
+// "33" => 33 - number
+// "33abc" => NaN
+// null => 0
+// true => 1 ? false => 0
+
+let loggedIn = ''
+let booleanLoggedIn = Boolean(loggedIn)
+console.log(typeof booleanLoggedIn);
+
+// 1 => true and 0 => false
+// "" => false
+// "rony" => true
