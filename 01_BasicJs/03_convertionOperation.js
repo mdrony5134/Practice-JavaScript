6ae0fdeabcb58d9481c7730dcddad58937abda0d
mdrony5134 <rony830388@gamil.com>
2023-10-04 12:07:53 +0600

    operations

--- 01_BasicJs/03_convertionOperation.js
+++ 01_BasicJs/03_convertionOperation.js
@@ -14,8 +14,37 @@ let scoreNumber = Number(score)
 
 let loggedIn = ''
 let booleanLoggedIn = Boolean(loggedIn)
-console.log(typeof booleanLoggedIn);
+// console.log(typeof booleanLoggedIn);
 
 // 1 => true and 0 => false
 // "" => false
 // "rony" => true
+
+// ************* Operations ************************
+
+let str1 = 'Hello'
+let str2 = 'rony'
+let str3 = "hello" + " rony"
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(1 + + 1 + "2")
+// console.log("1" + 1 + 2)
+
+// console.log(+true);
+// console.log(+"");
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
