16c09ee124f7f95faf2085115b9c66c1426625fc
mdrony5134 <rony830388@gamil.com>
2023-10-05 14:49:06 +0600

    learning numbers and math methods

---
+++ 01_BasicJs/06_number_Math.js
@@ -0,0 +1,31 @@
+let score = 400
+// console.log(score);
+let number = new Number(100)
+// console.log(number);
+// console.log(number.toLocaleString().length)
+// console.log(number.toFixed(2)) 
+// const newNumber = 1125.54112
+// console.log(newNumber.toPrecision(5))
+
+const balance = 100000000
+// console.log(balance.toLocaleString(''))
+
+
+//********************* Math ************************ */
+
+// console.log(Math)
+// console.log(Math.abs(-5)) //just negative value k return kore positive but positive value is positive
+// console.log(Math.round(4.5)) //Returns a supplied numeric expression rounded to the nearest integer.
+// console.log(Math.floor(4.8)) //Returns the greatest integer less than or equal to its numeric argument.
+// console.log(Math.ceil(4.58)) //Returns the smallest integer greater than or equal to its numeric argument.
+// console.log(Math.max(1,2,5,10,3,7))
+// console.log(Math.min(1,2,5,10,3,7))
+
+// console.log(Math.random()) //Returns a pseudorandom number between 0 and 1.
+
+// console.log(Math.floor(Math.random() * 6 )+ 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min) + 1) + min)
