c61b01d1575a91d32199ebec9c00927e3400959f
mdrony5134 <rony830388@gamil.com>
2023-10-05 16:17:36 +0600

    basic-2 learning  array

---
+++ 02_BasicsJs/Array.js
@@ -0,0 +1,26 @@
+// Array start
+const numberArr = [1,2,3,5,6]
+// console.log(numberArr[0])
+
+const friendsArr = ["hites", "nites", "challu", "kallu"]
+// console.log(friendsArr)
+
+//   numberArr.push(5)
+// numberArr.pop()
+// numberArr.unshift(9)
+// numberArr.shift()
+// const newArr = numberArr.join()
+// console.log(newArr)
+// console.log(numberArr)
+ 
+//slice and splice -----important
+console.log("A", numberArr)
+const myn1 = numberArr.slice(1, 3)
+console.log(myn1)
+
+console.log("B", numberArr)
+const myn2 = numberArr.splice(1, 3)
+console.log(myn2)
+console.log("C" , numberArr)
+
+
