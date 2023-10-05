0d1574f306845b140ca7e4bea2eda01dfb3d1d2c
mdrony5134 <rony830388@gamil.com>
2023-10-05 15:20:17 +0600

    learning dates methods

---
+++ 01_BasicJs/07_Date.js
@@ -0,0 +1,18 @@
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+// console.log(typeof myDate) // object
+
+// let createdDate = new Date(2020, 0, 15)
+// let createdDate = new Date('2020-01-15')
+let createdDate = new Date('01-15-2023')
+// console.log(createdDate.toLocaleString())
+
+// let timeStamp = Date.now()
+// console.log(timeStamp)
+// console.log(createdDate.getTime())
+
+console.log(Math.floor(Date.now() / 1000))
